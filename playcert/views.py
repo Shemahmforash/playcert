1c48a01cc4ad841518433a65829ed5e4cc78dfe3
Carlos Rosão <shemahmforash@hotmail.com>
2015-05-09 15:29:46 +0000

    Set the events in redis so that one does not need to use the eventful api constantly

--- playcert/views.py
+++ playcert/views.py
@@ -1,9 +1,8 @@
 from pyramid.view import view_config
-
-import os
-import eventful
+import os, eventful, redis, datetime, json
 
 api = eventful.API(os.environ['EVENTFUL_KEY'])
+redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
@@ -11,5 +10,13 @@ def my_view(request):
 
 @view_config(route_name='events', renderer='templates/events.pt')
 def events_view(request):
-    events = api.call('/events/search', c='music', l='Lisbon', date='This week')
+    today = datetime.date.today()
+    #get events from redis
+    events = redisClient.get('events.' + str(today))
+    events = json.loads(events) if events else ''
+    if not events :
+        #or get the events from the eventful api
+        events = api.call('/events/search', c='music', l='Lisbon', date='This week')
+        redisClient.set('events.' + str(today), json.dumps(events))
+
     return {'events': events['events']['event']}
