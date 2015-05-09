827a1bf799099455c023dcdd0d5d06a329310ed5
Carlos Rosão <shemahmforash@hotmail.com>
2015-05-09 12:19:39 +0000

    First tests with calling eventful api

--- playcert/views.py
+++ playcert/views.py
@@ -1,6 +1,15 @@
 from pyramid.view import view_config
 
+import os
+import eventful
+
+api = eventful.API(os.environ['EVENTFUL_KEY'])
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'playcert'}
+
+@view_config(route_name='events', renderer='templates/events.pt')
+def events_view(request):
+    events = api.call('/events/search', c='music', l='Lisbon', date='This week')
+    return {'events': events['events']['event']}
