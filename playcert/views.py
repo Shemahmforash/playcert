7b4595268d8f7e1bf18fd24225e91096b451deee
Carlos Rosão <shemahmforash@hotmail.com>
2015-05-10 19:05:41 +0000

    For the artist in each event, get his tracks from the echonest api

--- playcert/views.py
+++ playcert/views.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
 import os, eventful, redis, datetime, json
+from pyechonest import config, artist
 
+config.ECHO_NEST_API_KEY=os.environ['ECHONEST_KEY']
 api = eventful.API(os.environ['EVENTFUL_KEY'])
 redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -19,4 +21,26 @@ def events_view(request):
         events = api.call('/events/search', c='music', l='Lisbon', date='This week')
         redisClient.set('events.' + str(today), json.dumps(events))
 
-    return {'events': events['events']['event']}
+    events = process_events(events)
+
+    return {'events': events}
+
+def process_events(events):
+    processed = []
+    artists = []
+
+    for event in events['events']['event']:
+        event_processed = {}
+        event_processed['title'] = event['title']
+        event_processed['venue'] = event['venue_name']
+        event_processed['artist'] = ''
+
+        if isinstance(event['performers'], dict):
+            event_processed['artist'] = event['performers']['performer']['name']
+            #TODO: find artist from spotify
+            #event_processed['artist_info'] = artist.Artist(event_processed['artist'])
+            artist_info = artist.Artist(event_processed['artist'], bucket="id:spotify-WW")
+
+        processed.append(event_processed)
+
+    return processed
