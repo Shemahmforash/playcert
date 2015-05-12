b09827881a95bca625a5d9e7cc3637d7a5bbce90
Carlos Rosão <shemahmforash@hotmail.com>
2015-05-12 12:05:50 +0100

    Also set on redis songs by artist in order not to have to use the echonest api all the time to get this info

--- playcert/views.py
+++ playcert/views.py
@@ -19,9 +19,8 @@ def events_view(request):
     if not events :
         #or get the events from the eventful api
         events = api.call('/events/search', c='music', l='Lisbon', date='This week')
+        events = process_events(events)
         redisClient.set('events.' + str(today), json.dumps(events))
-
-    events = process_events(events)
 
     return {'events': events}
 
@@ -39,7 +38,25 @@ def process_events(events):
             event_processed['artist'] = event['performers']['performer']['name']
             #TODO: find artist from spotify
             #event_processed['artist_info'] = artist.Artist(event_processed['artist'])
-            artist_info = artist.Artist(event_processed['artist'], bucket="id:spotify-WW")
+            #artist_info = artist.Artist(event_processed['artist'], bucket="id:spotify-WW")
+
+            artist_info = redisClient.hget('artist', event_processed['artist'])
+#            artist_info = json.loads(artist_info) if artist_info else ''
+
+            if not artist_info :
+                results = artist.search(name=event_processed['artist'], buckets="id:spotify-WW")
+                artist_info = results[0]
+                redisClient.hset('artists', event_processed['artist'], artist_info)
+                redisClient.hset('songs', event_processed['artist'], artist_info.songs)
+
+            print artist_info.name, artist_info.id
+            print dir(artist_info.songs[0])
+            print 'Songs of:' + event_processed['artist'], [song.title for song in artist_info.songs]
+            """
+            weezer = weezer_results[0]
+            weezer_blogs = weezer.blogs
+            print 'Blogs about weezer:', [blog.get('url') for blog in weezer_blogs]
+            """
 
         processed.append(event_processed)
 
