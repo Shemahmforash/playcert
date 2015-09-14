e5fac591a91cea7f2c6db7c752a64eef1208e4e3
Carlos Rosão <shemahmforash@hotmail.com>
2015-09-14 15:28:12 +0000

    Code cleaning in generating event list.

--- playcert/views.py
+++ playcert/views.py
@@ -4,7 +4,6 @@ import os
 import eventful
 import redis
 import datetime
-import json
 import logging
 import re
 import sys
@@ -143,8 +142,9 @@ def find_artist_and_songs(event, response):
             log.debug('echonest_songs')
             log.debug(echonest_songs)
 
+            tracks = []
+
             if echonest_songs:
-                tracks = []
                 for s in echonest_songs:
                     t = s.get_tracks('spotify-WW')[0]
 
@@ -165,61 +165,37 @@ def create_playlist(events):
     """ Creates a playlist string to be rendered
     """
 
+    log.info('create_playlist')
+
     ids = []
     for event in events:
+        # log.info('event %s', event.title)
+
         if not event.artist:
             continue
 
         artist = event.artist
 
+        # log.info('artist %s', artist.name)
+
         if artist.songs:
             track = random.choice(artist.songs)
+
+            # log.info('track %s - %s', track.name, track.spotifyId)
+
             ids.append(track.spotifyId)
 
     ids = ','.join(ids)
+
+    # log.info('ids - %s', ids)
 
     # remove reference to spotify:track
     pattern = re.compile('spotify\:track\:')
     ids = pattern.sub('', ids)
 
+    log.info('ids - %s', ids)
+
     return ids
-
-
-@view_config(route_name='events', renderer='templates/events.pt')
-def events_view(request):
-    today = datetime.date.today()
-    location = 'Lisbon'
-    events_redis_key = location + '.events.' + str(today)
-
-    log.debug('location %s', location)
-
-    # get events from redis
-    events = redisClient.get(events_redis_key)
-    events = json.loads(events) if events else ''
-    if not events:
-        # or get the events from the eventful api
-        events = api.call(
-            '/events/search', c='music', l=location, date='This week'
-        )
-        log.debug('api response: %s', events)
-
-        events = process_events(events)
-        log.debug('processed_events: %s', events)
-
-        # and set them on redis
-        redisClient.set(events_redis_key, json.dumps(events))
-
-    log.debug('songs: %s', events['songs'])
-
-    playlist = generate_playlist(events['songs'])
-
-    log.debug('Playlist: %s', playlist)
-
-    return {
-        'events': events['events'],
-        'songs': events['songs'],
-        'playlist': playlist
-    }
 
 
 def generate_playlist(songs):
@@ -237,98 +213,3 @@ def generate_playlist(songs):
     ids = pattern.sub('', ids)
 
     return ids
-
-
-def filter_songs(songs, artist):
-    """Filters songs retrieving just the important fields
-    """
-
-    filtered_songs = []
-    for s in songs:
-        track = s.get_tracks('spotify-WW')[0]
-
-        simple_song = {'title': s.title, 'foreign_id': track[
-            'foreign_id'], 'artist': artist}
-        filtered_songs.append(simple_song)
-
-    return filtered_songs
-
-
-def find_artist(event):
-    if isinstance(event['performers'], dict):
-        return event[
-            'performers']['performer']['name']
-
-    # get artist from echonest api
-    try:
-        event_artist = pyechonest.artist.extract(
-            text=event['title'], results=1)
-    except:
-        log.error('could not find artist %s', sys.exc_info()[0])
-        return
-
-    log.debug('Event artist from echonest: %s', event_artist)
-    if event_artist:
-        return event_artist[0].name
-
-    return
-
-
-def process_events(events):
-    """ Transforms the events returned from the
-     eventful api in something meaningful
-    """
-    processed = []
-    global_songs = []
-
-    for event in events['events']['event']:
-        event_processed = {}
-        event_processed['title'] = event['title']
-        event_processed['venue'] = event['venue_name']
-        event_processed['when'] = event['start_time']
-        event_processed['artist'] = ''
-        event_processed['songs'] = ''
-
-        event_artist = find_artist(event)
-
-        if event_artist:
-            event_processed['artist'] = event_artist
-
-            # get artist songs from redis hash
-            songs = redisClient.hget('artist.songs', event_processed['artist'])
-            songs = json.loads(songs) if songs else ''
-
-            if not songs:
-                # or use the echonest api to find songs for an artist
-                # TODO: get artist from thisdayinmusic api
-                try:
-                    echonest_songs = pyechonest.song.search(
-                        artist=event_processed['artist'],
-                        buckets=['id:spotify-WW', 'tracks'],
-                        limit=True, results=1)
-                except:
-                    log.error(
-                        'could not find songs in echonest %s',
-                        sys.exc_info()[0])
-
-                if echonest_songs:
-                    # filter song data
-                    simplified_songs = filter_songs(
-                        echonest_songs, event_processed['artist'])
-
-                    event_processed['songs'] = simplified_songs
-                    global_songs = global_songs + simplified_songs
-
-                    # set the artist songs on a redis hash
-                    redisClient.hset(
-                        'artist.songs',
-                        event_processed['artist'],
-                        json.dumps(simplified_songs)
-                    )
-            else:
-                global_songs = songs
-                event_processed['songs'] = songs
-
-        processed.append(event_processed)
-
-    return {'events': processed, 'songs': global_songs}
