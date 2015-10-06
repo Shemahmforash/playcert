043791895087f612af88051b4561bb869bbd12e1
Carlos RosÃ£o <shemahmforash@hotmail.com>
2015-10-06 14:34:12 +0100

    artist cache in event class is now implemented with a decorator

--- playcert/lib/event.py
+++ playcert/lib/event.py
@@ -1,5 +1,6 @@
 import artist
 import dill
+from functools import wraps
 
 
 class Event:
@@ -9,22 +10,42 @@ class Event:
         self.when = when
         self.venue = venue
 
-        # if cache, try to get text artist from cache
-        if redis:
-            artist_cache = redis.hget('text.artist', self.title)
-            artist_cache = dill.loads(artist_cache) if artist_cache else ''
-            if artist_cache:
-                self.artist = artist_cache
-                return
+        # to use cache in this class (not mandatory)
+        if not redis is None:
+            self.redis = redis
 
+        # finds artist from event title
+        self.find_artist()
+
+    def cache_artist(f):
+        '''
+        Caches the artist on redis
+        '''
+        @wraps(f)
+        def decorated_function(*args):
+            self = args[0]
+
+            if self.redis:
+                artist_cache = self.redis.hget('text.artist', self.title)
+                artist_cache = dill.loads(artist_cache) if artist_cache else ''
+                if artist_cache:
+                    self.artist = artist_cache
+                    return
+
+            f(*args)
+
+            if self.artist and self.redis:
+                # set artist on cache
+                self.redis.hset(
+                    'text.artist', self.title, dill.dumps(self.artist))
+
+        return decorated_function
+
+    @cache_artist
+    def find_artist(self):
         # finds and creates artist from event title
         self.artist = artist.Artist.create_artist_from_text(
-            title, venue, redis)
-
-        # set artist and text correspondence on cache if there is cache
-        if redis and self.artist:
-            # set artist on cache
-            redis.hset('text.artist', self.title, dill.dumps(self.artist))
+            self.title, self.venue, self.redis)
 
     def __repr__(self):
         return 'Event(%s, %s, %s, %s)' % \
